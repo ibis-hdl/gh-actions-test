d62f8c1b6fd3c85abf160a2c07afb9c04b5553d4
Olaf <olaf@home.lan>
2022-06-13 19:45:05 +0200

    action test, using cpp-linter python package
    - changed main.cpp to trigger
    - using cpp-linter python package [header file not found #65](
      https://github.com/cpp-linter/cpp-linter-action/issues/65)
    - in cmake.yml, using hashFiles('conanfile.txt') since it's on root
    - simplification

--- source/main.cpp
+++ source/main.cpp
@@ -5,9 +5,14 @@
 #include <fmt/ostream.h>
 
 #include <iostream>
+#include <string_view>
 
-int main()
-{
-    std::cout << fmt::format("Hello {0}!\n", "World");
+void print(std::ostream& os, std::string_view name) {
+    os << fmt::format("Hello {0}!\n", name);
+}
+
+
+int main() {
+    print(std::cout, "World");
     return 0;
 }

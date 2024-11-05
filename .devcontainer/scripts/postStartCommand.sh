c94ec3162cf9c1f66b23c522c456a311171854d9
ibis-hdl <83083329+ibis-hdl@users.noreply.github.com>
2024-11-05 18:06:26 +0000

    fix linux-clang-libc++-release

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -12,12 +12,20 @@ configure_conan() {
     CXX=clang++ conan profile detect --force --name clang
     CXX=g++ conan profile detect --force --name gcc
     conan profile detect --force --name default
+    # special for libc++
+    CXX=clang++ conan profile detect --force --name clang-libc++
+    sed -i ~/.conan2/profiles/clang-libc++ -E \
+        -e "s|^(compiler\.cppstd\s*)=(.*)$|\1=17|g" \
+        -e "s|^(compiler\.libcxx\s*)=(.*)$|\1=libc++|g"
+
 }
 
 conan_install() {
     # apply project's Conan2 recipe for Release, Debug build
     conan install . --settings=compiler.cppstd=17 -s build_type=Release --output-folder build/conan --build=missing
     conan install . --settings=compiler.cppstd=17 -s build_type=Debug   --output-folder build/conan --build=missing
+    # special for libc++
+    CXX=clang++ conan install . -s compiler.cppstd=17 --output-folder build/conan --build=missing --profile:all=clang-libc++
 }
 
 install_conan

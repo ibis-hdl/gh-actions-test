646de036e5db7043729e828a96f9bb5f3d1714e2
ibis-hdl <83083329+ibis-hdl@users.noreply.github.com>
2024-11-06 19:18:15 +0000

    update creating conan profiles

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -7,25 +7,59 @@ install_conan() {
     pip3 --disable-pip-version-check --no-cache-dir install conan
 }
 
-configure_conan() {
-    # create compiler preset for Conan2
-    CXX=clang++ conan profile detect --force --name clang
-    CXX=g++ conan profile detect --force --name gcc
-    conan profile detect --force --name default
-    # special for libc++
-    CXX=clang++ conan profile detect --force --name clang-libc++
-    sed -i ~/.conan2/profiles/clang-libc++ -E \
-        -e "s|^(compiler\.cppstd\s*)=(.*)$|\1=17|g" \
-        -e "s|^(compiler\.libcxx\s*)=(.*)$|\1=libc++|g"
+# helper to adjust concrete compiler to be used
+conan_buildenv_conf()
+{
+    local CC=$1
+    local CXX=$2
+    cat << EOF
 
+[buildenv]
+CC=$CC
+CXX=$CXX
+
+[conf]
+tools.build:compiler_executables={ "c": "$(which $CC)", "cpp": "$(which $CXX)" }
+EOF
+}
+
+configure_conan() {
+    # create default compiler preset for Conan2
+    conan profile detect --force --name default
+
+    # create compiler preset for Conan2
+    local profile=gcc
+    CXX=g++ conan profile detect --force --name ${profile}
+    cat << EOF  >> ~/.conan2/profiles/${profile}
+$(conan_buildenv_conf 'gcc' 'g++')
+EOF
+    cat ~/.conan2/profiles/${profile}
+
+    # create compiler preset for Conan2
+    local profile=clang
+    CXX=clang++ conan profile detect --force --name ${profile}
+    cat << EOF  >> ~/.conan2/profiles/${profile}
+$(conan_buildenv_conf 'clang' 'clang++')
+EOF
+    cat ~/.conan2/profiles/${profile}
+
+    # # create compiler preset for Conan2 with special for libc++
+    local profile=clang-libc++
+    CXX=clang++ conan profile detect --force --name ${profile}
+    sed -i ~/.conan2/profiles/${profile} -E \
+        -e "s|^(compiler\.libcxx\s*)=(.*)$|\1=libc++|g"
+    cat << EOF  >> ~/.conan2/profiles/${profile}
+$(conan_buildenv_conf 'clang' 'clang++')
+EOF
+    cat ~/.conan2/profiles/${profile}
 }
 
 conan_install() {
     # apply project's Conan2 recipe for Release, Debug build
-    conan install . --settings=compiler.cppstd=17 -s build_type=Release --output-folder build/conan --build=missing
-    conan install . --settings=compiler.cppstd=17 -s build_type=Debug   --output-folder build/conan --build=missing
+    conan install . -s build_type=Release --output-folder build/conan --build=missing --profile:all=default
+    conan install . -s build_type=Debug   --output-folder build/conan --build=missing --profile:all=default
     # special for libc++
-    CXX=clang++ conan install . -s compiler.cppstd=17 --output-folder build/conan --build=missing --profile:all=clang-libc++
+    conan install . -s build_type=Release --output-folder build/conan --build=missing --profile:all=clang-libc++
 }
 
 install_conan
